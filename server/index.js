d9f44658c12214bf17efb138b2cff20462d247df
Kunal Rana <kunalranabsr9458@gmail.com>
2025-04-09 13:56:04 +0530

    changes in cors for backend and frontend integration

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://edtechplatformfrontend.vercel.app",
         credentials:true,
     })
 )
